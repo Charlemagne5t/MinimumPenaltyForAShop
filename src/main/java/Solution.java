e6397e7eb5dbb894f3b2901fb056b3e42e382955
alek <wait82985@gmail.com>
2023-08-29 21:48:04 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,28 @@
 public class Solution {
     public int bestClosingTime(String customers) {
-        return 0;
+        char[] customersPresent = customers.toCharArray();
+        int yCount = 0;
+        for (int i = 0; i < customersPresent.length; i++) {
+            if(customersPresent[i] == 'Y'){
+                yCount++;
+            }
+        }
+        int currentMinPenalty = yCount;
+        int minPenaltyHour = 0;
+        int globalMinCount = yCount;
+
+        for (int i = 1; i < customersPresent.length + 1; i++) {
+            if(customersPresent[i - 1] == 'Y'){
+                currentMinPenalty--;
+                if(currentMinPenalty < globalMinCount){
+                    globalMinCount = currentMinPenalty;
+                    minPenaltyHour = i;
+                }
+            }else {
+                currentMinPenalty++;
+            }
+        }
+
+        return minPenaltyHour;
     }
 }
