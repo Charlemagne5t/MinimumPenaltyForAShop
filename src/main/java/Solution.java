79fad81bb8948b28b195cb5f108f86aaf4d9e1fd
alek <wait82985@gmail.com>
2023-08-29 21:52:47 +0600

    reduce runtime. 1 pass

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,28 +1,24 @@
 public class Solution {
     public int bestClosingTime(String customers) {
         char[] customersPresent = customers.toCharArray();
-        int yCount = 0;
-        for (int i = 0; i < customersPresent.length; i++) {
-            if(customersPresent[i] == 'Y'){
-                yCount++;
-            }
-        }
-        int currentMinPenalty = yCount;
-        int minPenaltyHour = 0;
-        int globalMinCount = yCount;
+
+
+        int currentMaxScore = 0;
+        int maxScoreHour = 0;
+        int globalMaxCount = 0;
 
         for (int i = 1; i < customersPresent.length + 1; i++) {
             if(customersPresent[i - 1] == 'Y'){
-                currentMinPenalty--;
-                if(currentMinPenalty < globalMinCount){
-                    globalMinCount = currentMinPenalty;
-                    minPenaltyHour = i;
+                currentMaxScore++;
+                if(currentMaxScore > globalMaxCount){
+                    globalMaxCount = currentMaxScore;
+                    maxScoreHour = i;
                 }
             }else {
-                currentMinPenalty++;
+                currentMaxScore--;
             }
         }
 
-        return minPenaltyHour;
+        return maxScoreHour;
     }
 }
